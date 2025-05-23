206d7496f18643e575b6edd2d4117d70175f6386
Diniz018 <italo.dnz01@gmail.com>
2025-05-23 12:32:54 -0300

    .

---
+++ funcao/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+int main(int argc, char* argv[]){
+
+    if(argc < 3){
+        printf("Argumentos Invalidos");
+        return 1;
+    }
+
+    if(strcmp(argv[1], "-maior") == 0){
+     if(argc != 4){
+         printf("Argumentos Invalidos");
+         return 1;
+     }
+
+     int x= atoi(argv[2]);
+     int y= atoi(argv[3]);
+     printf("%d", maior_2n(x,y));
+
+    }
+
+    if(strcmp(argv[1], "-primo") == 0){
+        if(argc == 3){
+            printf("Argumentos Invalidos");
+            return 1;
+        }
+
+        int x = atoi(argv[2]);
+    }
+
+    return 0;
+
+}    
